ad02e1bba70eed2397f7f65cef031c397a82bc3f
Julio Souza <94882193+Julio-o-Julio@users.noreply.github.com>
2025-06-05 21:58:34 -0400

    Adicionanodo comentarios semanticos para passar no test do pylint

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
-# app.py
+"""
+Módulo que contém funções matemáticas simples.
+"""
 
 def soma(a, b):
+    """
+    Função que recebe dois números e retorna a soma deles.
+
+    Parâmetros:
+    a (int, float): O primeiro número.
+    b (int, float): O segundo número.
+
+    Retorna:
+    int, float: A soma de a e b.
+    """
     return a + b
--- test_app.py
+++ test_app.py
@@ -1,4 +1,11 @@
+"""
+Testes para o módulo app.py.
+"""
+
 from app import soma
 
 def test_soma():
+    """
+    Testa a função soma, verificando se a soma de 7 e 10 é 17.
+    """
     assert soma(7, 10) == 17
